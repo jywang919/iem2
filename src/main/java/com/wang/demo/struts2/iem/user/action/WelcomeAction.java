0ee5d6da9d39fd6846239573a2403d56cc4a3ce8
jiyongwangMBPro <jiyongwangMBPro@macbook-pro>
2019-08-23 16:57:00 -0400

    added login failure logic

---
+++ src/main/java/com/wang/demo/struts2/iem/user/action/WelcomeAction.java
@@ -0,0 +1,37 @@
+package com.wang.demo.struts2.iem.user.action;
+
+public class WelcomeAction{
+
+    private final static String SUCCESS = "SUCCESS";
+    private final static String FAILURE = "FAILURE";
+    
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// all struts logic here
+	public String execute() {
+        if (null != this.getUsername() && null != this.getPassword()
+        		&& this.getUsername().length() > 0 && this.getPassword().length() > 0
+        		&& this.getUsername().equals(this.getPassword())) {
+            return SUCCESS;
+        }
+		return FAILURE;
+
+	}
+}
---
+++ src/main/webapp/User/pages/failure.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Struts 2 Failure</title>
+    </head>
+    <body>
+        <div style="color:red">
+            <h1>Invalid login info for user:  <s:property value="username"/></h1>
+        </div>
+    </body>
+</html>
